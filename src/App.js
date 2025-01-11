fca4afdc193ec0f5a8133c63b39c4edfcdd7913d
“asif” <mail2asifsana@gmail.com>
2025-01-11 22:45:59 +0600

    A nice clock added --fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [time, setTime] = useState(0);
